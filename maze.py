a93635f294783c726571ecb7f33d8c1015de1b8f
ghnjk <guohainan@guohainandeMacBook-Pro.local>
2018-04-19 01:32:44 +0800

    add q-learning sarsa

---
+++ maze.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*- 
+import random
+
+class MazeEnv(object):
+
+    def __init__(self, row_count, col_count, block_rate = 0.3):
+        self.row_count = row_count
+        self.col_count = col_count
+        self.init_pos = [random.randint(0, row_count - 1), random.randint(0, col_count - 1)]
+        while True:
+            self.target_pos = [random.randint(0, row_count - 1), random.randint(0, col_count - 1)]
+            if self.target_pos == self.init_pos:
+                continue
+            else:
+                break
+        self.mp = []
+        for r in range(self.row_count):
+            self.mp.append([])
+            for c in range(self.col_count):
+                if random.uniform(0, 1) <= block_rate:
+                    s = '#'
+                else:
+                    s = '.'
+                if [r, c] == self.init_pos:
+                    s = '.'
+                elif [r, c] == self.target_pos:
+                    s = 'T'
+                self.mp[r].append(s)
+        self.reset_env()
+
+    def reset_env(self):
+        self.cur_pos = list(self.init_pos)
+
+    def __str__(self):
+        res = ""
+        for r in range(self.row_count):
+            s = ""
+            for c in range(self.col_count):
+                if [r, c] == self.cur_pos:
+                    s += "S"
+                elif [r, c] == self.target_pos:
+                    s += 'T'
+                else:
+                    s += self.mp[r][c]
+            res += s + "\n"
+        return res
+
+    def get_state(self):
+        return (self.cur_pos[0] + 1) * (self.col_count + 2) + self.cur_pos[1] + 1
+
+    def get_all_actions(self):
+        return ['R', 'L', 'U', 'D']
+
+    def do_action(self, action):
+        if action == 'R':
+            self.cur_pos[1] += 1
+        elif action == 'L':
+            self.cur_pos[1] -= 1
+        elif action == 'U':
+            self.cur_pos[0] -= 1
+        elif action == 'D':
+            self.cur_pos[0] += 1
+        else:
+            print "invalid action: " + str(action)
+        if self.cur_pos[0] < 0 or self.cur_pos[0] >= self.row_count:
+            s = '#'
+        elif self.cur_pos[1] < 0 or self.cur_pos[1] >= self.col_count:
+            s = '#'
+        else:
+            s = self.mp[self.cur_pos[0]][self.cur_pos[1]]
+        if s == '#':
+            reward = -1
+            is_terminate = True
+        elif s == 'T':
+            reward = 1
+            is_terminate = True
+        else:
+            reward = 0
+            is_terminate = False
+        return (self.get_state(), is_terminate, reward)
---
+++ q_learning.py
@@ -0,0 +1,125 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*- 
+
+import numpy as np
+import pandas as pd
+import random
+import time
+import matplotlib.pyplot as plt
+from maze import * 
+
+def set_rand_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+
+class QLearning(object):
+
+    def __init__(self, actions):
+        self.table = pd.DataFrame(
+            columns = actions
+            , dtype=np.float64
+            )
+        self.actions = actions
+
+    def choose_action(self, state, epsilon = 0.9):
+        self.check_state_exist(state)
+        if np.random.uniform() > epsilon:
+            return np.random.choice(self.actions)
+        else:
+            state_action = self.table.loc[state, :]
+            #同一个 state, 可能会有多个相同的 Q action value, 所以我们乱序一下
+            state_action = state_action.reindex(np.random.permutation(state_action.index))
+            return state_action.idxmax()
+
+    def learn(self, cur_state, action, next_state, reward, learning_rate = 0.01, reward_decent_rate = 0.9):
+        predict_value = self.table.loc[cur_state, action]
+        if next_state is None:
+            target = reward
+        else:
+            self.check_state_exist(next_state)
+            target = reward + reward_decent_rate * self.table.loc[next_state, :].max()
+        self.table.loc[cur_state, action] += learning_rate * (target - predict_value)
+
+    def check_state_exist(self, state):
+        if state not in self.table.index:
+            self.table = self.table.append(
+                pd.Series(
+                    [0] * len(self.actions),
+                    index = self.table.columns,
+                    name = state,
+                )
+            )
+
+
+
+def play(env, ql):
+    env.reset_env()
+    is_terminate = False
+    while not is_terminate:
+        s = env.get_state()
+        action = ql.choose_action(s, 1.0)
+        print str(env)
+        print "action: " + action
+        s, is_terminate , _ = env.do_action(action)
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    set_rand_seed(7)
+    # 不好搞定
+    env = MazeEnv(20, 30, block_rate = 0.25)
+    #env = MazeEnv(19, 19, block_rate = 0.25)
+    ql = QLearning(env.get_all_actions())
+    epoch_count = 1000
+    avg_rate = []
+    avg_len = []
+    all_reward = 0
+    all_step = 0
+    cnt = 0
+    for epoch in range(epoch_count):
+        env.reset_env()
+        s = env.get_state()
+        is_terminate = False
+        step_list = []
+        while not is_terminate:
+            action = ql.choose_action(s)
+            #print "action: " + str(action)
+            (ns, is_terminate, reward) = env.do_action(action)
+            # 使用这种更新效率比较底下
+            #ql.learn(s, action, ns, reward)
+            step_list.append((s, action, ns, reward))
+            s = ns
+        for i in range(len(step_list) - 1, -1, -1):
+            s, action, ns, _ = step_list[i]
+            ql.learn(s, action, ns, reward / float(len(step_list) - i))
+        all_reward += reward
+        all_step += len(step_list)
+        cnt += 1
+        if epoch % (epoch_count / 100) == 0:
+            avg_r = all_reward / float(cnt)
+            avg_s = all_step / float(cnt)
+            all_reward = 0
+            all_step = 0
+            cnt = 0
+            avg_rate.append(avg_r)
+            avg_len.append(avg_s)
+            print "epoch %d avg_s [%0.4lf] avg_r [%0.4lf]" % (epoch, avg_s, avg_r)
+    env.reset_env()
+    print "------------"
+    print str(env)
+    print "------------"
+    play(env, ql)
+    #print "q_tables:"
+    #print ql.table
+    f = plt.figure()
+    plt_1 = f.add_subplot(111)
+    plt_1.plot(range(len(avg_rate)), avg_rate, 'r', label="average reward")
+    plt_2 = plt_1.twinx()
+    plt_2.plot(range(len(avg_len)), avg_len, 'b--', label="average steps")
+    plt.legend()
+    plt.show()
+
+
+
+
+
